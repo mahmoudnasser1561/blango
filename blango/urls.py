531f8bfe4951ec2ad18becefbbeb9f9748ea926f
mahmoudnasser1561 <mahmoudnasser1561@gmail.com>
2024-08-24 06:55:26 +0300

    Finish html frameworks

--- blango/urls.py
+++ blango/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from blog import views
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import blog.views
 
 urlpatterns = [
+    # path('blog/', include('blog.index.html')),
     path('admin/', admin.site.urls),
+    path('', views.index),
 ]
